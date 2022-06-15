db86c3c61cacf3eeac40e9560c56a37bad053aa6
Tawatchai Suktrakul <Tawatchai.Suktrakul@gmail.com>
2022-06-15 20:39:48 +0700

    Implement service register at root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -10,7 +9,6 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +24,7 @@ import { ServersService } from './servers/servers.service';
     BrowserModule,
     FormsModule,
   ],
-  providers: [ServersService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
